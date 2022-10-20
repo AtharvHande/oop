c4734883dba378fc4df8e83ff07e22dd423dfd3c
AtharvHande <116243023+AtharvHande@users.noreply.github.com>
2022-10-20 09:00:35 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+import myPackage.*;
+
+
+class Test
+{
+public static void main(String args[])
+{
+Student s1=new Student();
+s1.display();
+
+Studentmarks s2=new Studentmarks();
+s2.displayMarks();
+
+}
+}
